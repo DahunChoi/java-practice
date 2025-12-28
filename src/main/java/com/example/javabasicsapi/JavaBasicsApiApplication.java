b55ee4eb1480d927bf14c96b17bd673aabbd9240
DahunChoi <31028816+DahunChoi@users.noreply.github.com>
2025-12-28 22:23:56 +0900

    [#1] User API 기본 구조 및 CRUD 기능 구현

---
+++ src/main/java/com/example/javabasicsapi/JavaBasicsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabasicsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBasicsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javabasicsapi/JavaBasicsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabasicsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
