99d2a4bbdc7d7ffce45f78b2b613649465639972
dahunchoi <dh95322@naver.com>
2026-01-17 22:21:22 +0900

    Feat: CacheConfig 추가

---
+++ src/main/java/com/example/javabasicsapi/config/CacheConfig.java
@@ -0,0 +1,17 @@
+package com.example.javabasicsapi.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+    public static final String PRODUCTS_CACHE = "products";
+
+    @Bean
+    public CacheManager cacheManager() {
+        // 기본 In-Memory 캐시 (ConcurrentHashMap 기반)
+        return new ConcurrentMapCacheManager(PRODUCTS_CACHE);
+    }
+}
