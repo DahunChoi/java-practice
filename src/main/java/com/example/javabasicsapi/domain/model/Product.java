b8e1609431de123992e6cd6dd63355afddc1bc88
dahunchoi <dh95322@naver.com>
2026-01-17 22:26:54 +0900

    Feat: Product.java 추가

---
+++ src/main/java/com/example/javabasicsapi/domain/model/Product.java
@@ -0,0 +1,24 @@
+package com.example.javabasicsapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class Product {
+    private final Long id;
+
+    @Setter
+    private String name;
+
+    @Setter
+    private BigDecimal price;
+
+    @Setter
+    private int stock;
+}
