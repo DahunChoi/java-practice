fb2e0464521dd63b40edf8acfed61b4726214798
dahunchoi <dh95322@naver.com>
2026-01-18 00:03:16 +0900

    Feat: ProductController.java 추가

---
+++ src/main/java/com/example/javabasicsapi/presentation/ProductController.java
@@ -0,0 +1,48 @@
+package com.example.javabasicsapi.presentation;
+
+import com.example.javabasicsapi.application.dto.ProductCreateRequest;
+import com.example.javabasicsapi.application.dto.ProductResponse;
+import com.example.javabasicsapi.application.dto.ProductUpdateRequest;
+import com.example.javabasicsapi.application.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse create(@RequestBody @Valid ProductCreateRequest req) {
+        return productService.create(req);
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponse get(@PathVariable Long id) {
+        return productService.getById(id);
+    }
+
+    @GetMapping
+    public List<ProductResponse> getAll() {
+        return productService.getAll();
+    }
+
+    @PutMapping
+    public ProductResponse update(@PathVariable Long id, @RequestBody @Valid ProductUpdateRequest req) {
+        return productService.update(id, req);
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        productService.delete(id);
+
+    }
+}
